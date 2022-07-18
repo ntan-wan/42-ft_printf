e8d3789198c7b17450ed45ff7601677e40211df3
ntan-wan <ntan-wan@AMYs-MacBook-Air.local>
2022-07-18 10:35:12 +0800

    passed and norminette-ed everything

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,18 +6,22 @@
 /*   By: ntan-wan <ntan-wan@42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 17:41:18 by ntan-wan          #+#    #+#             */
-/*   Updated: 2022/07/18 01:34:04 by ntan-wan         ###   ########.fr       */
+/*   Updated: 2022/07/18 10:32:07 by ntan-wan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 #include "../libft/libft.h"
 
-void	fmt_process(va_list args, t_fmt *fmt, int *print_len)
+int	fmt_process(const char *str, size_t *ptr_i, va_list args)
 {
-	//check_flags(str, i, fmt);
-	(*print_len) += fmt_operation(fmt, args);
-	//fmt_reset(&fmt);
+	t_fmt	fmt;
+	int		print_len;
+
+	fmt_init(&fmt);
+	fmt_update(str[(*ptr_i) + 1], &fmt);
+	print_len = fmt_operation(&fmt, args);
+	return (print_len);
 }
 
 int	ft_printf(const char *str, ...)
@@ -25,26 +29,19 @@ int	ft_printf(const char *str, ...)
 	size_t	i;
 	va_list	args;
 	int		print_len;
-	t_fmt	fmt;
 
 	i = 0;
 	print_len = 0;
-	//fmt_init(&fmt);
 	va_start(args, str);
 	while (str[i])
 	{
 		if (str[i] == '%')
 		{
-			fmt_init(&fmt);
-			fmt_update(str[i + 1], &fmt);
-			fmt_process(args, &fmt, &print_len);
+			print_len += fmt_process(str, &i, args);
 			i++;
 		}
 		else
-		{
-			ft_putchar_fd(str[i], 1);
-			print_len++;
-		}
+			print_len += print_c(str[i]);
 		i++;
 	}
 	va_end(args);
