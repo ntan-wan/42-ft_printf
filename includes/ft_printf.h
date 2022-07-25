cfe7128c5a9410d969c5059f0deb517d5b39455c
Ngan Tan Wang <ntan-wan@u81z06s03.42kl.edu.my>
2022-07-25 10:44:57 +0800

    norminetted header file, everything completed

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: ntan-wan <ntan-wan@42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/14 09:18:31 by ntan-wan          #+#    #+#             */
-/*   Updated: 2022/07/24 22:20:43 by ntan-wan         ###   ########.fr       */
+/*   Updated: 2022/07/25 10:41:39 by ntan-wan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@
 
 # include <stddef.h>
 
-int		ft_printf(const char *str, ...);
+int				ft_printf(const char *str, ...);
 
-int		flag_check_bonus(char c);
+int				flag_check_bonus(char c);
 
 typedef struct s_fmt
 {
@@ -50,38 +50,38 @@ typedef struct s_fmt
 	int		offset;
 }	t_fmt;
 
-void	fmt_init(t_fmt *fmt);
-
-void	fmt_update(char c, t_fmt *fmt);
-
-void	fmt_update_flags(char c, t_fmt *fmt);
-
-int		fmt_operation(t_fmt *fmt, va_list args);
-
-int		print_c(t_fmt *fmt, int c);
-
-int		print_s(t_fmt *fmt, char *str);
-
-int		print_ptr(t_fmt *fmt, unsigned long long ptr);
-
-int		print_decimal(t_fmt *fmt, int num);
-
-int		print_hexa(t_fmt *fmt, unsigned int num);
-
-int		print_percent(t_fmt *fmt);
-
-int		print_unsi(t_fmt *fmt, unsigned int unsi);
-
-void	print_prefix_unsi(t_fmt *fmt, char *unsi_num_c);
-
-void	print_prefix_nbr(t_fmt *fmt, int num, char *unsi_num_c);
-
-char	*ft_uitoa(unsigned int n);
-
-int		calc_unsi_num_len(unsigned int num, int base);
-
-void	print_space(t_fmt *fmt, int space_count);
-
 unsigned int	absolute(int num);
+
+void			fmt_init(t_fmt *fmt);
+
+void			fmt_update(char c, t_fmt *fmt);
+
+void			fmt_update_flags(char c, t_fmt *fmt);
+
+int				fmt_operation(t_fmt *fmt, va_list args);
+
+int				print_c(t_fmt *fmt, int c);
+
+int				print_s(t_fmt *fmt, char *str);
+
+int				print_ptr(t_fmt *fmt, unsigned long long ptr);
+
+int				print_decimal(t_fmt *fmt, int num);
+
+int				print_hexa(t_fmt *fmt, unsigned int num);
+
+int				print_percent(t_fmt *fmt);
+
+int				print_unsi(t_fmt *fmt, unsigned int unsi);
+
+void			print_prefix_unsi(t_fmt *fmt, char *unsi_num_c);
+
+void			print_prefix_nbr(t_fmt *fmt, int num, char *unsi_num_c);
+
+char			*ft_uitoa(unsigned int n);
+
+int				calc_unsi_num_len(unsigned int num, int base);
+
+void			print_space(t_fmt *fmt, int space_count);
 
 #endif
